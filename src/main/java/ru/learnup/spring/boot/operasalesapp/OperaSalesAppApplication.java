56872b470a60ab514e06fe93b3496043a6cf7d29
Asbest96 <t549nk@mail.ru>
2022-02-14 22:01:25 +0500

    startversion

---
+++ src/main/java/ru/learnup/spring/boot/operasalesapp/OperaSalesAppApplication.java
@@ -0,0 +1,13 @@
+package ru.learnup.spring.boot.operasalesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperaSalesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperaSalesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/learnup/spring/boot/operasalesapp/OperaSalesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.learnup.spring.boot.operasalesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperaSalesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
